757b7c57591c876477af720ad05c73c719213540
goodbbak <wndks8564@naver.com>
2023-03-27 17:51:32 +0900

    0327학원2

--- ex230323/src/main/java/com/yedam/spring/board/service/BoardService.java
+++ ex230323/src/main/java/com/yedam/spring/board/service/BoardService.java
@@ -12,12 +12,12 @@ public interface BoardService {
 		//등록하고자 하는 글 번호
 		public BoardVO getBoardNo();
 		
-		//등록
+		//등록 - 등록된 글 번호 반환 실패시 -1 반환
 		public int insertBoardInfo(BoardVO boardVO);
 		
-		//수정
+		//수정 - 수정된 글 번호 반환 실패시 -1 반환
 		public int updateBoardInfo(BoardVO boardVO);
 		
-		//삭제
+		//삭제 - 삭제된 글 번호 반환 실패시 -1 반환
 		public int deleteBoardInfo(int BoardNo);
 }
